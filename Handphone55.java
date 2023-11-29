44f982f38dfb7bf7fc8cf6c843d1e0fa0a52296b
bintangnusantara388 <145913395+bintangnusantara388@users.noreply.github.com>
2023-11-29 20:15:25 +0700

    Update Handphone55.java

--- Handphone55.java
+++ Handphone55.java
@@ -1,7 +1,11 @@
 /*
+ * Nama : Bintang Fajar Nusantara
+ * Nim : 22205028
+ * Prodi : Teknik Informatika
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package hanphone55;
 
 public class Handphone55 {
